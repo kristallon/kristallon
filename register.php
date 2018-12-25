968c4bd86f5e395ca777fddff955bb2daed5a26f
kristallon <46134312+kristallon@users.noreply.github.com>
2018-12-25 16:40:12 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://gneeebot.herokuapp.com/execute.php';
+$WEBHOOK_URL = 'https://kristallon.herokuapp.com/execute.php';
 $BOT_TOKEN = '738136947:AAHueqE1KPZeQ4FAIblODg4qlsY-zghikIM';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
